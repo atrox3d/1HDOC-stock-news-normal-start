273c15ed73c56a7c59636f39f9fd53b614f2681b
atrox3d <atrox3d@gmail.com>
2021-02-25 07:56:31 +0100

    add myob private data

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from myob import myob
+
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = myob.STOCK_API_KEY
 
 ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
