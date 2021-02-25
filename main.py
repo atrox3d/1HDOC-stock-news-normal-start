49b6f2699041b556f7f7f5d1370b71d15982efc3
atrox3d <atrox3d@gmail.com>
2021-02-25 08:26:56 +0100

    TODO 5

--- main.py
+++ main.py
@@ -42,7 +42,9 @@ print(positive_difference)
 diff_percent = (positive_difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
-# TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+# 5. - If TODO4 percentage is greater than 5 then print("Get News").
+if diff_percent > 5:
+    print("Get News")
 
 ## STEP 2: https://newsapi.org/
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
